a244dbd12d161c61f532ad5a9aaa3ae249785fb8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2020-01-15 21:46:19 +0300

    Added InitializationOrder task project

---
+++ InitializationOrderTask/Abstract/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InitializationOrderTask.Abstract
+{
+    public abstract class Car
+    {
+        private int _speed;
+
+        public Car(int maxSpeed)
+        {
+            MaxSpeed = 10;
+        }
+
+        public int MaxSpeed { get; }
+
+        public void Drive()
+        {
+            if (_speed > 0)
+            {
+                Console.WriteLine($"Driving with speed {_speed}");
+            }
+        }
+
+        public void Accelerate()
+        {
+            if (_speed < MaxSpeed)
+            {
+                _speed++;
+                Console.WriteLine($"Accelerating to speed {_speed}");
+            }
+            else
+            {
+                Console.WriteLine($"Car is driving at maximum speed {MaxSpeed}");
+            }
+        }
+
+        public void Decelerate()
+        {
+            _speed--;
+            Console.WriteLine($"Decelerating to speed {_speed}");
+        }
+    }
+}
---
+++ InitializationOrderTask/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InitializationOrderTask
+{
+    // 1. Создай класс Lamborgini и унаследуй его от класса Car
+    // Lamborgini должен иметь максимальную скорость 300.
+    // Создай объект Lamborgini и протестируй его максимальную скорость.
+    // Исправь ошибку в программе.
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
