fc35f3f03061564e4c2fb147f0bf85991e87696c
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2020-01-22 19:34:36 +0300

    unfinished, just save to repo

---
+++ GenericsTasks/Entities/Classes/Match.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities.Classes
+{
+    public class Match: Entity
+    {
+        public DateTime timeOfStart = new DateTime();
+        public int Duration { get; set; }
+        public string Result { get; set; }
+        public Team team1 = new Team();
+        public Team team2 = new Team();
+    }
+}
---
+++ GenericsTasks/Entities/Classes/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Classes
+{
+    public class Player : Entity
+    {
+        public string Name { get; set; }
+        public Team team = new Team();
+        List<Match> matches = new List<Match>();
+    }
+}
